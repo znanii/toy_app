6980003ebd2844a4b1a3f96458643401d2f5a2c2
zhanii <naborznanii@gmail.com>
2024-04-11 21:47:55 -0700

    scaffold Micropost + db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_12_042942) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_12_044616) do
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
