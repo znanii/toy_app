551df83b20ca36f2f9d77f43e676b5346702be4c
zhanii <naborznanii@gmail.com>
2024-04-11 22:19:15 -0700

    more validation

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,6 @@
 class Micropost < ApplicationRecord
-  validates :content, length: { maximum: 140 }
-  
+  validates :content, length: { maximum: 140 }, presence: true
+  validates :user_id, presence: true
+
   belongs_to :user
 end
